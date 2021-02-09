0419bfc956698328d01a275858623919fd70a59d
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-02-09 17:58:18 +0100

    Simple interaction system with a test script : InteractionTestScript. I used an abstract class in order to disallow multiple uses of interaction scripts on a single asset.

---
+++ Snowy-Path/Assets/Scripts/InteractionSystem/InteractionTestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTestScript : InteractionBase {
+
+    public Light cubeLight;
+    private bool _isBlue = false;
+
+    private Color _baseColor;
+
+    void Start() {
+        _baseColor = cubeLight.color;
+    }
+
+    public override void Interact() {
+
+        if (!_isBlue) {
+            cubeLight.color = Color.blue;
+        } else {
+            cubeLight.color = _baseColor;
+        }
+        _isBlue = !_isBlue;
+    }
+}
