0face9ed0a9c63e4dfd5dc4f5e93b87d52f11e7e
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-02-09 20:32:46 +0100

    Added regions & comments

--- Snowy-Path/Assets/Scripts/InteractionSystem/InteractionBase.cs
+++ Snowy-Path/Assets/Scripts/InteractionSystem/InteractionBase.cs
@@ -2,7 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Abstract class for any interaction script.
+/// Provides abstract methods for derived classes in order to be interactable.
+/// </summary>
 [DisallowMultipleComponent]
 public abstract class InteractionBase : MonoBehaviour {
+
+    /// <summary>
+    /// Do the interaction.
+    /// </summary>
     public abstract void Interact();
+
+    /// <summary>
+    /// Show the interaction feedback.
+    /// Cound be any feedback (UI, VFX, SFX ...)
+    /// </summary>
+    public abstract void ShowInteractionFeedback();
+
+    /// <summary>
+    /// Hide the interaction feedback.
+    /// </summary>
+    public abstract void HideInteractionFeedback();
 }
