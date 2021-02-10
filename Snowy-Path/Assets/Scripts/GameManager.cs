fd46f1bd6ba8292c000669dc82190803156a15af
Az <thomas.guyamier@gmail.com>
2021-02-10 16:57:38 +0100

    Add GameManager

---
+++ Snowy-Path/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    // Player
+    internal PlayerController player;
+
+    private void Awake() {
+
+        // Singleton of the GameManager
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(instance);
+            instance = gameObject.GetComponent<GameManager>();
+            return;
+        }
+
+        // 
+        player = FindObjectOfType<PlayerController>();
+    }
+}
