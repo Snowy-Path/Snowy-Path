31e8d03da6b6b2f93e0107d6b23f40942b7dd5c2
Az <thomas.guyamier@gmail.com>
2021-02-10 11:09:06 +0100

    Creation of a IData interface

---
+++ Snowy-Path/Assets/Scripts/Save/IData.cs
@@ -0,0 +1,12 @@
+﻿interface IData
+{
+    /// <summary>
+    /// Load the data in the Object
+    /// </summary>
+    public void LoadData();
+
+    /// <summary>
+    /// Get the data from the Object
+    /// </summary>
+    public void GetData();
+}
