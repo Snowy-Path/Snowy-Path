7d3a7ec8e9c2cb272b8e8f5896c33d432b8088fc
Az <thomas.guyamier@gmail.com>
2021-02-10 11:09:50 +0100

    Creation PlayerData
    Implementation of the IData interface

---
+++ Snowy-Path/Assets/Scripts/Save/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿
+[System.Serializable]
+public class PlayerData : IData
+{
+    public void LoadData() {
+        throw new System.NotImplementedException();
+    }
+
+    public void GetData() {
+        throw new System.NotImplementedException();
+    }
+}
