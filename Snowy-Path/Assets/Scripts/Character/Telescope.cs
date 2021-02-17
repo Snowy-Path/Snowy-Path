b40e99a8dc6cda90daa27846a6e0c13c644eaf3f
Hugo Quenon <quenon.hugo@gmail.com>
2021-02-17 15:44:11 +0100

    Telescope 1rst commit, two level of FOV, "Vision" tag visible when on scoped
    Main Interaction for scope mode and secondary to change magnification

---
+++ Snowy-Path/Assets/Scripts/Character/Telescope.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Telescope : MonoBehaviour
+{
+
+    public GameObject scopeOverlay; //Overlay effect on UI
+    public GameObject scopeCamera;
+    public Camera MainCamera;
+    public float magnification1;
+    public float magnification2;
+    private float normalFOV; //Basic Camera FOV
+    private bool isScoped;
+    private void Start()
+    {
+        scopeCamera.SetActive(true);
+        scopeCamera.SetActive(false);
+    }
+    void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+        if (keyboard.eKey.wasPressedThisFrame)
+        {
+            MainInteraction();
+        }
+        //If in scope mode, change FOV
+        if (keyboard.fKey.wasPressedThisFrame && isScoped)
+        {
+            SecondaryInteraction();
+        }
+    }
+    /// <summary>
+    /// If unscoped, active scope mode, else disactive it
+    /// </summary>
+    void MainInteraction()
+    {
+        isScoped = !isScoped;
+        if (isScoped)
+        {
+            StartCoroutine(OnScoped());
+
+        }
+        else
+        {
+            OnUnscoped();
+
+        }
+
+    }
+    /// <summary>
+    /// Change FOV to zoom further
+    /// </summary>
+    void SecondaryInteraction()
+    {
+        MainCamera.fieldOfView = magnification2;
+    }
+
+    /// <summary>
+    /// Change FOV back to normal and disable Overlay
+    /// </summary>
+    void OnUnscoped()
+    {
+        //
+        scopeOverlay.SetActive(false);
+        scopeCamera.SetActive(true);
+        MainCamera.fieldOfView = normalFOV;
+
+    }
+
+    /// <summary>
+    /// Change
+    /// </summary>
+    /// <param name="maglevel"></param>
+    /// <returns></returns>
+    IEnumerator OnScoped()
+    {
+        yield return new WaitForSeconds(.15f);
+        //Activate overlay and "Vision" Layer objects
+        scopeOverlay.SetActive(true);
+        scopeCamera.SetActive(false);
+        //Change FOV
+        normalFOV = MainCamera.fieldOfView;
+        MainCamera.fieldOfView = magnification1;
+
+    }
+
+}
