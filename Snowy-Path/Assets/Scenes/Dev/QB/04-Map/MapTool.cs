399ef9be01dfb22028becdd4c8ecb79291d71059
Quentin Bazin <quent42340@gmail.com>
2021-02-17 16:40:46 +0100

    [Map] Added map canvas with toggleable full screen map display.

---
+++ Snowy-Path/Assets/Scenes/Dev/QB/04-Map/MapTool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MapTool : MonoBehaviour
+{
+    public GameObject mapUI;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard.mKey.wasPressedThisFrame) {
+            mapUI.SetActive(!mapUI.activeSelf);
+        }
+    }
+}
