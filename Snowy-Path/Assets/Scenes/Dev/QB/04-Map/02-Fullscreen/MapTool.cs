17d28899e5263e5d8388f64864e1f0ae99c935d1
Quentin Bazin <quent42340@gmail.com>
2021-02-19 15:27:10 +0100

    [Map] Now changing input mode in fullscreen mode.
    Also release the mouse, and recapture it when exiting the mode.

---
+++ Snowy-Path/Assets/Scenes/Dev/QB/04-Map/02-Fullscreen/MapTool.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MapTool : MonoBehaviour
+{
+    public GameObject mapUI;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard.mKey.wasPressedThisFrame) {
+            mapUI.SetActive(!mapUI.activeSelf);
+
+            if (mapUI.activeSelf) {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                GetComponentInParent<PlayerInput>().SwitchCurrentActionMap("UI");
+            }
+            else {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                GetComponentInParent<PlayerInput>().SwitchCurrentActionMap("Gameplay");
+            }
+        }
+    }
+}
