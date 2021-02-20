0ba051198fdbbe87f767aae957d6de07dd4df17f
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-02-20 15:33:39 +0100

    Powder creation menu & prefab.

--- Snowy-Path/Assets/Scripts/Editor/CreationMenus/PowderCreationMenus.cs
+++ Snowy-Path/Assets/Scripts/Editor/CreationMenus/PowderCreationMenus.cs
@@ -4,20 +4,20 @@ using UnityEditor;
 using UnityEngine;
 
 /// <summary>
-/// Represent the Campfire prefab creation menu under "GameObjects".
-/// It holds every pre-defined Campfire prefab instantiation methods.
+/// Represent the Powder prefabs creation menu under "GameObjects > Powders".
+/// It holds every pre-defined Powder prefabs instantiation methods.
 /// </summary>
-public class CampfireCreationMenu {
+public class PowderCreationMenu {
 
     /// <summary>
-    /// Instantiate a Campfire Prefab with the Interactable script.
+    /// Instantiate a Powder Prefab with the Interactable script.
     /// </summary>
     /// <param name="menuCommand"></param>
-    [MenuItem("GameObject/Campfire/Campfire", false, 10)]
-    public static void CreateCampfire(MenuCommand menuCommand) {
+    [MenuItem("GameObject/Powders/Powder", false, 10)]
+    public static void CreatePowder(MenuCommand menuCommand) {
 
         // LightCloth prefab
-        GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Campfire.prefab", typeof(GameObject)));
+        GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Powders/Powder.prefab", typeof(GameObject)));
 
         // Position
         MenuUtility.PlaceInFrontOfCamera(go);
