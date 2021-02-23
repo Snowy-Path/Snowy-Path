0e25ffdf8fc176d7f125e6acdfa9b856a48da6d5
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-02-23 17:41:04 +0100

    Animation takes both arms. The left one is pulled back a bit when attacking. The right one does a large swing when attacking.

--- Snowy-Path/Assets/Scripts/Tool/Torch.cs
+++ Snowy-Path/Assets/Scripts/Tool/Torch.cs
@@ -7,28 +7,21 @@ public class Torch : MonoBehaviour
 {
 
     [Min(0)]
-    public float attackRange = 1.0f; //Range from animation ???
-
-    [Min(0)]
     public int attackDamage = 1;
 
     [Min(0)]
     public float attackRecovery = 0.2f;
-    private float attackRecoveryTimer = 0f;
-
-    [Min(0)]
-    public float castAttack = 0.4f;
 
     public Animator animator;
 
-    public void MaintInteraction(InputAction.CallbackContext context) {
+    public void MainInteraction(InputAction.CallbackContext context) {
         if (context.phase == InputActionPhase.Performed) {
             PerformAttack();
         }
     }
 
     private void PerformAttack() {
-        animator.SetTrigger("Attack");
+        animator.SetTrigger("BaseAttack");
     }
 
     private void OnTriggerEnter(Collider other) {
