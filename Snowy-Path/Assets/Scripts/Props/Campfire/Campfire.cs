b3e684966d74721c1d975ce6bdb1d66d6070a394
Az <thomas.guyamier@gmail.com>
2021-02-24 11:15:26 +0100

    Added unity event + prefab campfire updated
    - Unity event for ignite and extinguish event
    - Setup the prefab for the unity event

--- Snowy-Path/Assets/Scripts/Props/Campfire/Campfire.cs
+++ Snowy-Path/Assets/Scripts/Props/Campfire/Campfire.cs
@@ -1,30 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Campfire : MonoBehaviour
 {
-
     [Tooltip("Time for the fire to extinguish")]
     public float extinctionTime = 0f;
 
     [Tooltip("Radius of the extinction range")]
     public float radius = 0f;
 
-    private float extinctionFireTimer = 0f;
+    [Header("UnityEvents callbacks")]
 
-    // Stopping timer when fire is not active
-    private bool isFireActive = false;
+    [Tooltip("Callbacks called when the fire ignites.")]
+    public UnityEvent onIgnite;
+
+    [Tooltip("Callbacks called when the fire extinguish.")]
+    public UnityEvent onExtinguish;
 
     public bool showDebug = false;
 
     private HeatSource heatSource;
 
+    private float extinctionFireTimer = 0f;
+
+    // Stopping timer when fire is not active
+    private bool isFireActive = false;
+
+
     private void Start() {
         heatSource = GetComponent<HeatSource>();
         heatSource.enabled = false;
-        GetComponentInChildren<MeshRenderer>().materials[1].color = new Color(0, 0, 0);
-
     }
 
     private void Update() {
@@ -46,20 +53,18 @@ public class Campfire : MonoBehaviour
     /// Call every method necessary when interacting with the campfire
     /// </summary>
     public void IgniteFire() {
-        GetComponentInChildren<MeshRenderer>().materials[1].color = new Color(1, 0, 0);
         isFireActive = true;
+        onIgnite.Invoke();
         heatSource.enabled = true;
     }
 
     /// <summary>
-    /// Call every method necessary when the campfire need to be exctinguished
+    /// Call every method necessary when the campfire need to be extinguished
     /// </summary>
     internal void ExctinguishFire() {
-        GetComponentInChildren<MeshRenderer>().materials[1].color = new Color(0,0,0);
         isFireActive = false;
+        onExtinguish.Invoke();
         heatSource.enabled = false;
-
-
     }
 
     void OnDrawGizmos() {
