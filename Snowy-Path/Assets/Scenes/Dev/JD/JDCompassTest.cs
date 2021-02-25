4c956f40584747a61a053fa4f9a5df4cc8cfcb9e
Gen0902 <50872015+Gen0902@users.noreply.github.com>
2021-02-25 11:55:35 +0100

    Update handcontroller
    Type => EToolType
    Add get current tool prop
    Add secondary use

--- Snowy-Path/Assets/Scenes/Dev/JD/JDCompassTest.cs
+++ Snowy-Path/Assets/Scenes/Dev/JD/JDCompassTest.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JDCompassTest : MonoBehaviour, IHandTool {
+    public EToolType ToolType { get => EToolType.MapCompass; }
 
-    public void PrimaryUse() {
+    public void StartPrimaryUse() {
         Locate();
+    } 
+    
+    public void CancelPrimaryUse() {
+        Debug.Log("Stop using map and compass");
     }
+
+    public void SecondaryUse() {
+        Debug.Log("Start editing mode");
+    }
+
     public void ToggleDisplay(bool display) {
         gameObject.SetActive(display);
     }
