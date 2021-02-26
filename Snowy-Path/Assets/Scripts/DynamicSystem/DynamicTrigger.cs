c6ce99a0c6c0dbc98002dc476deaa4f7b1471d94
Gen0902 <50872015+Gen0902@users.noreply.github.com>
2021-02-26 16:39:36 +0100

    Add decription in DynamicEvent & CustomEditor

--- Snowy-Path/Assets/Scripts/DynamicSystem/DynamicTrigger.cs
+++ Snowy-Path/Assets/Scripts/DynamicSystem/DynamicTrigger.cs
@@ -9,14 +9,14 @@ public class DynamicTrigger : MonoBehaviour {
 
         if (other.gameObject.tag == "Player" && onEnterDynamicEvent.dynamicEvent != null) {
             DynamicSystem.RegisterEvent(onEnterDynamicEvent); //Send dynamic event
-            Debug.Log($"Firing {onEnterDynamicEvent.dynamicEvent}={onEnterDynamicEvent.eventStatus}");
+            Debug.Log($"Firing [{onEnterDynamicEvent.dynamicEvent}]=[{onEnterDynamicEvent.eventStatus}]");
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.tag == "Player" && onExitDynamicEvent.dynamicEvent != null) {
             DynamicSystem.RegisterEvent(onExitDynamicEvent); //Send dynamic event
-            Debug.Log($"Firing {onEnterDynamicEvent.dynamicEvent}={onExitDynamicEvent.eventStatus}");
+            Debug.Log($"Firing [{onExitDynamicEvent.dynamicEvent}]=[{onExitDynamicEvent.eventStatus}]");
         }
     }
 }
