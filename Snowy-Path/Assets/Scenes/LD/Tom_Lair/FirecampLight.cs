56c20f6e8f1aaeabd528814d876315cc85111f85
Tom Parseihian <tom.parseihian@gmail.com>
2021-03-01 16:22:03 +0100

    Level antre wendigo (#38)
    * Created branch
    
    * Installed ProBuilder, Polybrush & Terrain Tools
    
    * Numeroted maze platforms
    
    * Cave entrance shape
    
    * Added firecamp
    
    * Added materials
    
    * First corridor ground
    
    * Made path from 1 to 2 and 1 to 3
    
    * Paths connected until crossing 7
    
    * Path until crossing 13
    
    * Moved crossing 17
    
    * Almost finished cave ground
    
    * Cave ground finished
    
    * Removed unecessary faces on ground geometry
    
    * Added beginning of walls
    
    * Made roof of the beginning of the cave
    
    * Begin addition of props
    
    * Remade the cave entrance
    
    * Added firecamp + clothe at the cave entrance
    
    * Added first room of labyrinth
    
    * Added room 3
    
    * Added room 3
    
    * Added props in cave entrance
    
    * aDDED ROOM 4
    
    * Added old cave as reference
    
    * Added props in all the existing rooms
    
    * Added room 5
    
    * Added room 6
    
    * Added room 7
    
    * Added room 8
    
    * Connected 8 to 2
    
    * Fixed too small corridors
    
    * Added rooms until 13
    
    * Added all missing labyrinth rooms
    
    * Connected every rooms
    
    * Polybrushed until room 14
    
    * Begin to place assets
    
    * Finished placing rock assets
    
    * Added blood
    
    * Final room added
    
    * WendigosCave done
    
    * Added lights
    
    * Finished blocking
    
    * Prepared scene for light baking
    
    * Baked lighting
    
    * Set player at start position

---
+++ Snowy-Path/Assets/Scenes/LD/Tom_Lair/FirecampLight.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirecampLight : MonoBehaviour
+{
+    Light light;
+    float timer;
+    bool shouldGrow;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        timer = Random.Range(.2f, .6f);
+        shouldGrow = Random.value > .5;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = Random.Range(.2f, .6f);
+            shouldGrow = !shouldGrow;
+        }
+
+        if(shouldGrow)
+        {
+            light.range += Random.Range(-.1f * Time.deltaTime, .5f * Time.deltaTime) * 5;
+            light.intensity += Random.Range(-.1f * Time.deltaTime, .5f * Time.deltaTime) * 5;
+        }
+        else
+        {
+            light.range -= Random.Range(-.1f * Time.deltaTime, .5f * Time.deltaTime) * 5;
+            light.intensity -= Random.Range(-.1f * Time.deltaTime, .5f * Time.deltaTime) * 5;
+        }
+
+        Mathf.Clamp(light.range, 2, 12);
+        Mathf.Clamp(light.intensity, .8f, 1.8f);
+    }
+}
+
+
+
+/*
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirecampLight : MonoBehaviour
+{
+    Light light;
+    public float minRange = 1, maxRange = 10;
+    public float minIntensity = .5f, maxIntensity = 3;
+    public float speed = 50f;
+    bool shouldGrow;
+    bool shouldGlow;
+
+    float targetRange;
+    float targetIntensity;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        shouldGrow = true;
+        targetRange = maxRange + Random.Range(-3, 3);
+        targetIntensity = maxIntensity + Random.Range(-.2f, .8f);
+    }
+
+    void Update()
+    {
+        if(shouldGrow)
+        {
+            if (light.range < targetRange)
+            {
+                light.range += Random.Range(-.1f, .3f) * Time.deltaTime * speed;
+            }
+            else
+            {
+                shouldGrow = false;
+                targetRange = minRange + Random.Range(-3, 3);
+            }
+        }
+        else
+        {
+            if (light.range > targetRange)
+            {
+                light.range -= Random.Range(-.1f, .3f) * Time.deltaTime * speed;
+            }
+            else
+            {
+                shouldGrow = true;
+                targetRange = minRange + Random.Range(-3, 3);
+            }
+        }
+
+        if(shouldGlow)
+        {
+            if (light.intensity < targetIntensity)
+            {
+                light.intensity += Random.Range(-.01f, .03f) * Time.deltaTime * speed;
+            }
+            else
+            {
+                shouldGlow = false;
+                targetIntensity = minIntensity + Random.Range(-.2f, .8f);
+            }
+        }
+        else
+        {
+            if (light.intensity > targetIntensity)
+            {
+                light.intensity -= Random.Range(-.01f, .03f) * Time.deltaTime * speed;
+            }
+            else
+            {
+                shouldGlow = true;
+                targetIntensity = maxIntensity + Random.Range(-.2f, .8f);
+            }
+        }
+    }
+}
+*/
