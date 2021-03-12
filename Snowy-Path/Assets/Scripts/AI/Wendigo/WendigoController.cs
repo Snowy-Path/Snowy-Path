1041093eaa4c93f8b1dcc5e7831df1a4df106a26
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-03-12 17:54:40 +0100

    NavMesh generated for both Wolf & Wendigo. Wendigo follow the player

---
+++ Snowy-Path/Assets/Scripts/AI/Wendigo/WendigoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WendigoController : MonoBehaviour {
+
+    private NavMeshAgent m_agent;
+    private Transform m_player;
+
+    void Start() {
+        m_player = FindObjectOfType<PlayerController>().transform;
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        m_agent.SetDestination(m_player.position);
+    }
+}
