d6bb78098440d0f004e3cfccdc5a9a8993da690f
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-03-15 16:11:51 +0100

    Wendigo instant kill the player.

--- Snowy-Path/Assets/Scripts/AI/EnnemyAttack.cs
+++ Snowy-Path/Assets/Scripts/AI/EnnemyAttack.cs
@@ -9,15 +9,21 @@ public class EnnemyAttack : MonoBehaviour {
 
     [Tooltip("Damages dealt to player.")]
     [Min(0)]
+    [SerializeField]
     public int attackDamage = 1;
 
     [Tooltip("Percentage reduction to the current player's temperature.")]
     [Range(0, 1)]
+    [SerializeField]
     public float temperaturePercentageDamage = 0.1f;
 
     [Tooltip("Percentage reduction to the current player's durability.")]
     [Range(0, 1)]
+    [SerializeField]
     public float clothPercentageDamage = 0.1f;
+
+    [SerializeField]
+    public bool instantKill = false;
 
     /// <summary>
     /// When another colliders collides, it verifies if it is tagged "Player" and then apply damage to it.
@@ -25,9 +31,14 @@ public class EnnemyAttack : MonoBehaviour {
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            other.GetComponent<GenericHealth>().Hit(attackDamage);
+            if (instantKill) {
+                other.GetComponent<GenericHealth>().Hit(other.GetComponent<GenericHealth>().maxHealth);
+            } else {
+                other.GetComponent<GenericHealth>().Hit(attackDamage);
+            }
             other.GetComponent<Temperature>().ReduceCurrentTemperatureWithPercentage(temperaturePercentageDamage);
             other.GetComponent<Inventory>().ReduceClothDurabilityPercentage(clothPercentageDamage);
         }
     }
+
 }
---
+++ Snowy-Path/Assets/Scripts/Editor/AttackInspector/EnnemyAttackEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+[CustomEditor(typeof(EnnemyAttack))]
+public class EnnemyAttackEditor : Editor {
+    public override void OnInspectorGUI() {
+
+        var myScript = target as EnnemyAttack;
+
+        myScript.instantKill = GUILayout.Toggle(myScript.instantKill, "Instant kill");
+
+        if (!myScript.instantKill) {
+            myScript.attackDamage = EditorGUILayout.IntSlider("Attack damage:", myScript.attackDamage, 1, 100);
+        }
+
+        myScript.temperaturePercentageDamage = EditorGUILayout.Slider("Temperature %", myScript.temperaturePercentageDamage, 0, 1);
+        myScript.clothPercentageDamage = EditorGUILayout.Slider("Cloth %", myScript.clothPercentageDamage, 0, 1);
+
+        if (GUI.changed && !Application.isPlaying) {
+            EditorUtility.SetDirty(target);
+            EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+        }
+
+    }
+}
