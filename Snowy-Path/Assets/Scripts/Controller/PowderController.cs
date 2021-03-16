de44f4068e8adf83e376555be39a06f8943df7ee
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-03-16 14:27:38 +0100

    Interacting with a powder now changes the player's lights colors and particle system gradient

---
+++ Snowy-Path/Assets/Scripts/Controller/PowderController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowderController : MonoBehaviour {
+
+    #region Variables
+    public Color effectColor;
+    public Gradient particlesEffectGradient;
+
+    public Light torchLight;
+    private Color torchLightBaseColor;
+
+    public Light foreverLight;
+    private Color foreverLightBaseColor;
+
+    public ParticleSystem torchParticles;
+    private ParticleSystem.ColorOverLifetimeModule colorModule;
+    private Gradient torchParticlesBaseGradient;
+
+    private Coroutine effectCoroutine;
+
+    public float effectDuration;
+    #endregion
+
+
+    #region Built-In Methods
+    private void Start() {
+        torchLightBaseColor = torchLight.color;
+        foreverLightBaseColor = foreverLight.color;
+
+        colorModule = torchParticles.colorOverLifetime;
+        torchParticlesBaseGradient = colorModule.color.gradient;
+    }
+    #endregion
+
+
+    #region Powder Effect Methods
+    internal void ActivatePowderEffects() {
+        if (effectCoroutine != null) {
+            StopCoroutine(effectCoroutine);
+        }
+        effectCoroutine = StartCoroutine(ChangeColor());
+    }
+
+    private IEnumerator ChangeColor() {
+
+        torchLight.color = effectColor;
+        foreverLight.color = effectColor;
+        colorModule.color = particlesEffectGradient;
+
+        yield return new WaitForSeconds(effectDuration);
+
+        torchLight.color = torchLightBaseColor;
+        foreverLight.color = foreverLightBaseColor;
+        colorModule.color = torchParticlesBaseGradient;
+
+    }
+    #endregion
+
+}
---
+++ Snowy-Path/Assets/Scripts/Props/Powder/Powder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powder : MonoBehaviour {
+
+    public void ActivatePowderEffects() {
+        PowderController powderController = FindObjectOfType<PowderController>();
+        powderController.ActivatePowderEffects();
+    }
+
+}
