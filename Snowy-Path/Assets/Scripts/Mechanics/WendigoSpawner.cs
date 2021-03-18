688804b816fd9461e52fb59692c4588b6e1f338a
Tom Parseihian--Hoppenot <tom.parseihian@gmail.com>
2021-03-18 14:01:49 +0100

    Added scripts and triggers for spawning and despawning the Wendigo

---
+++ Snowy-Path/Assets/Scripts/Mechanics/WendigoSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WendigoSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject wendigo;
+
+    private void Start() {
+        wendigo.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            wendigo.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
