676aa7924c1cba7b30f634f82614a113363d10cf
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-03-19 16:55:51 +0100

    Blizzard fixed and everything :ceps:

--- Snowy-Path/Assets/Scripts/Environment/WeatherZone.cs
+++ Snowy-Path/Assets/Scripts/Environment/WeatherZone.cs
@@ -2,34 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeatherZone : MonoBehaviour
-{
-    public float radius = 10f;
-
+[RequireComponent(typeof(Collider))]
+public class WeatherZone : MonoBehaviour {
     public WeatherPreset weatherPreset;
-
-    public bool showDebug = false;
-
-    void Start()
-    {
-        // Initialize the object that will contain our trigger collider
-        GameObject child = new GameObject("WeatherRange");
-        child.tag = "WeatherZone";
-        child.transform.parent = transform;
-        child.transform.localPosition = Vector3.zero;
-
-        // Initialize the collider
-        SphereCollider sphereCollider = child.AddComponent<SphereCollider>();
-        sphereCollider.isTrigger = true;
-        sphereCollider.radius = radius;
-    }
-
-    void OnDrawGizmos()
-    {
-        if (showDebug) {
-            Gizmos.color = new Color(1, 0.5f, 0.5f, 0.25f);
-            Gizmos.DrawSphere(transform.position, radius);
-            Gizmos.DrawWireSphere(transform.position, radius);
-        }
-    }
 }
