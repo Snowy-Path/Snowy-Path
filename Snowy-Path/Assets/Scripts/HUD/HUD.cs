98e9e27ad7532aec2d265dd61058509e47692d34
Jer D <dambraij@gmail.com>
2021-03-23 17:52:25 +0100

    Added overlays

---
+++ Snowy-Path/Assets/Scripts/HUD/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] Image staminaOverlay;
+    [SerializeField] Image bloodOverlay;
+    [SerializeField] Image frozenOverlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetStamina(float amount) {
+
+    }
+
+    public void SetBlood(float amount) {
+
+    }
+
+    public void SetFrozen(float amount) {
+
+    }
+}
