71b5c959b43beb64c38b18b76b1fbc5c48987f5e
Quentin Bazin <quent42340@gmail.com>
2021-04-07 17:13:31 +0200

    [Gallery] Can now open a window by clicking on an image.

--- Snowy-Path/Assets/Scripts/UI/Gallery.cs
+++ Snowy-Path/Assets/Scripts/UI/Gallery.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Gallery : MonoBehaviour
 {
     public List<Color> imageList;
+    public GameObject galleryWindow;
 
     GameObject m_galleryContent;
 
@@ -13,12 +14,33 @@ public class Gallery : MonoBehaviour
     {
         m_galleryContent = GetComponentInChildren<GridLayoutGroup>().gameObject;
 
+        int i = 0;
         foreach (var image in imageList) {
             GameObject galleryImage = new GameObject("GalleryImage");
             galleryImage.transform.parent = m_galleryContent.transform;
 
             Image img = galleryImage.AddComponent<Image>();
             img.color = Random.ColorHSV(); // FIXME: image unused
+
+            int imageID = i;
+            Button btn = galleryImage.AddComponent<Button>();
+            btn.onClick.AddListener(() => OnImageClick(imageID));
+
+            ++i;
         }
+    }
+
+    void OnImageClick(int imageID)
+    {
+        if (!galleryWindow.activeSelf) {
+            galleryWindow.SetActive(true);
+        }
+    }
+
+    public void OnBackButtonClicked() {
+        if (galleryWindow.activeSelf)
+            galleryWindow.SetActive(false);
+        else
+            gameObject.SetActive(false);
     }
 }
