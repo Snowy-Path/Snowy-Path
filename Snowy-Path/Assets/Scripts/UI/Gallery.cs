5073c9d1d177a8e51bf3ba4a87896cc8ab1c1f8c
Quentin Bazin <quent42340@gmail.com>
2021-04-07 16:58:36 +0200

    [Gallery] First working version.

---
+++ Snowy-Path/Assets/Scripts/UI/Gallery.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gallery : MonoBehaviour
+{
+    public List<Color> imageList;
+
+    GameObject m_galleryContent;
+
+    void Start()
+    {
+        m_galleryContent = GetComponentInChildren<GridLayoutGroup>().gameObject;
+
+        foreach (var image in imageList) {
+            GameObject galleryImage = new GameObject("GalleryImage");
+            galleryImage.transform.parent = m_galleryContent.transform;
+
+            Image img = galleryImage.AddComponent<Image>();
+            img.color = Random.ColorHSV(); // FIXME: image unused
+        }
+    }
+}
