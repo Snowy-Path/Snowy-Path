3f29e0487abc3ece3f4fd2296ee8e2c4481a2d9f
Quentin Bazin <quent42340@gmail.com>
2021-04-07 18:20:42 +0200

    [Gallery] Now using GalleryElement to display data on gallery window.

--- Snowy-Path/Assets/Scripts/UI/Gallery.cs
+++ Snowy-Path/Assets/Scripts/UI/Gallery.cs
@@ -5,41 +5,56 @@ using UnityEngine.UI;
 
 public class Gallery : MonoBehaviour
 {
-    public List<Color> imageList;
-    public GameObject galleryWindow;
-
+    GalleryWindow m_galleryWindow;
     GameObject m_galleryContent;
+    Object[] m_galleryElements;
 
     void Start()
     {
+        m_galleryWindow = GetComponentInChildren<GalleryWindow>();
+        m_galleryWindow.gameObject.SetActive(false);
+
         m_galleryContent = GetComponentInChildren<GridLayoutGroup>().gameObject;
 
+        m_galleryElements = Resources.LoadAll<GalleryElement>("Gallery");
+        if (m_galleryElements.Length < 1) {
+            throw new System.Exception("Gallery folder is empty, or its path is wrong.");
+        }
+
         int i = 0;
-        foreach (var image in imageList) {
+        foreach (var elem in m_galleryElements) {
+            GalleryElement galleryElement = (GalleryElement)elem; 
+
             GameObject galleryImage = new GameObject("GalleryImage");
             galleryImage.transform.parent = m_galleryContent.transform;
 
-            Image img = galleryImage.AddComponent<Image>();
-            img.color = Random.ColorHSV(); // FIXME: image unused
+            RawImage img = galleryImage.AddComponent<RawImage>();
+            img.color = Color.white;
+            img.texture = galleryElement.texture;
 
-            int imageID = i;
+            int elementID = i;
             Button btn = galleryImage.AddComponent<Button>();
-            btn.onClick.AddListener(() => OnImageClick(imageID));
+            btn.onClick.AddListener(() => OnImageClick(elementID));
 
             ++i;
         }
     }
 
-    void OnImageClick(int imageID)
+    void OnImageClick(int elementID)
     {
-        if (!galleryWindow.activeSelf) {
-            galleryWindow.SetActive(true);
+        if (!m_galleryWindow.gameObject.activeSelf) {
+            var galleryElement = (GalleryElement)m_galleryElements[elementID];
+            m_galleryWindow.title.text = galleryElement.title;
+            m_galleryWindow.description.text = galleryElement.description;
+            m_galleryWindow.image.texture = galleryElement.texture;
+
+            m_galleryWindow.gameObject.SetActive(true);
         }
     }
 
     public void OnBackButtonClicked() {
-        if (galleryWindow.activeSelf)
-            galleryWindow.SetActive(false);
+        if (m_galleryWindow.gameObject.activeSelf)
+            m_galleryWindow.gameObject.SetActive(false);
         else
             gameObject.SetActive(false);
     }
---
+++ Snowy-Path/Assets/Scripts/UI/GalleryElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gallery/Element")]
+public class GalleryElement : ScriptableObject
+{
+    public string title;
+
+    [Multiline]
+    public string description;
+
+    public Texture texture;
+}
