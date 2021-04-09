8c177936c3f0cfe8a2426d7692a7296cded5afb2
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-04-09 15:20:31 +0200

    Wendigo use the correct speed when moving on a NavMeshLink. It also rotates with the correct angularSpeed. The Wendigo's angularSpeed is increased 3 times.

--- Snowy-Path/Assets/Scripts/AI/AgentLinkMover.cs
+++ Snowy-Path/Assets/Scripts/AI/AgentLinkMover.cs
@@ -2,11 +2,14 @@
 using UnityEngine.AI;
 using System.Collections;
 
+/// <summary>
+/// OffMeshLink movement method
+/// </summary>
 public enum OffMeshLinkMoveMethod {
     Teleport,
-    NormalSpeed,
-    Parabola,
-    Curve
+    NormalSpeed
+    //Parabola,
+    //Curve
 }
 
 /// <summary>
@@ -14,7 +17,7 @@ public enum OffMeshLinkMoveMethod {
 /// </summary>
 [RequireComponent(typeof(NavMeshAgent))]
 public class AgentLinkMover : MonoBehaviour {
-    public OffMeshLinkMoveMethod m_Method = OffMeshLinkMoveMethod.Parabola;
+    public OffMeshLinkMoveMethod m_Method = OffMeshLinkMoveMethod.NormalSpeed;
     public AnimationCurve m_Curve = new AnimationCurve();
 
     IEnumerator Start() {
@@ -24,48 +27,73 @@ public class AgentLinkMover : MonoBehaviour {
             if (agent.isOnOffMeshLink) {
                 if (m_Method == OffMeshLinkMoveMethod.NormalSpeed)
                     yield return StartCoroutine(NormalSpeed(agent));
-                else if (m_Method == OffMeshLinkMoveMethod.Parabola)
-                    yield return StartCoroutine(Parabola(agent, 2.0f, 0.5f));
-                else if (m_Method == OffMeshLinkMoveMethod.Curve)
-                    yield return StartCoroutine(Curve(agent, 0.5f));
+                //else if (m_Method == OffMeshLinkMoveMethod.Parabola)
+                //    yield return StartCoroutine(Parabola(agent, 2.0f, 0.5f));
+                //else if (m_Method == OffMeshLinkMoveMethod.Curve)
+                //    yield return StartCoroutine(Curve(agent, 0.5f));
                 agent.CompleteOffMeshLink();
             }
             yield return null;
         }
     }
 
+    /// <summary>
+    /// Move along the link using the agent's speed.
+    /// </summary>
+    /// <param name="agent">Agent to move.</param>
+    /// <returns></returns>
     IEnumerator NormalSpeed(NavMeshAgent agent) {
         OffMeshLinkData data = agent.currentOffMeshLinkData;
         Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        agent.updateRotation = false;
         while (agent.transform.position != endPos) {
+            RotateAgent(agent, endPos);
             agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
             yield return null;
         }
+        agent.updateRotation = true;
     }
 
-    IEnumerator Parabola(NavMeshAgent agent, float height, float duration) {
-        OffMeshLinkData data = agent.currentOffMeshLinkData;
-        Vector3 startPos = agent.transform.position;
-        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
-        float normalizedTime = 0.0f;
-        while (normalizedTime < 1.0f) {
-            float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
-            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
-            normalizedTime += Time.deltaTime / duration;
-            yield return null;
-        }
+    //IEnumerator Parabola(NavMeshAgent agent, float height, float duration) {
+    //    OffMeshLinkData data = agent.currentOffMeshLinkData;
+    //    Vector3 startPos = agent.transform.position;
+    //    Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+    //    float normalizedTime = 0.0f;
+    //    agent.updateRotation = false;
+    //    while (normalizedTime < 1.0f) {
+    //        RotateAgent(agent, endPos);
+    //        float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
+    //        agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+    //        normalizedTime += Time.deltaTime / duration;
+    //        yield return null;
+    //    }
+    //    agent.updateRotation = true;
+    //}
+
+    //IEnumerator Curve(NavMeshAgent agent, float duration) {
+    //    OffMeshLinkData data = agent.currentOffMeshLinkData;
+    //    Vector3 startPos = agent.transform.position;
+    //    Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+    //    float normalizedTime = 0.0f;
+    //    agent.updateRotation = false;
+    //    while (normalizedTime < 1.0f) {
+    //        RotateAgent(agent, endPos);
+    //        float yOffset = m_Curve.Evaluate(normalizedTime);
+    //        agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+    //        normalizedTime += Time.deltaTime / duration;
+    //        yield return null;
+    //    }
+    //    agent.updateRotation = true;
+    //}
+
+    /// <summary>
+    /// Rotate the agent to make it look at the end position of the OffMeshLink.
+    /// </summary>
+    /// <param name="agent">Agent to rotate.</param>
+    /// <param name="endPos">Position to look at.</param>
+    private void RotateAgent(NavMeshAgent agent, Vector3 endPos) {
+        Quaternion rotate = Quaternion.LookRotation(endPos - agent.transform.position);
+        agent.transform.rotation = Quaternion.Slerp(agent.transform.rotation, rotate, Time.deltaTime * Mathf.Deg2Rad * agent.angularSpeed);
     }
 
-    IEnumerator Curve(NavMeshAgent agent, float duration) {
-        OffMeshLinkData data = agent.currentOffMeshLinkData;
-        Vector3 startPos = agent.transform.position;
-        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
-        float normalizedTime = 0.0f;
-        while (normalizedTime < 1.0f) {
-            float yOffset = m_Curve.Evaluate(normalizedTime);
-            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
-            normalizedTime += Time.deltaTime / duration;
-            yield return null;
-        }
-    }
 }
