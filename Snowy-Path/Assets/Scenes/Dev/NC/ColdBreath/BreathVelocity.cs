1e4ebe041cb7d098358d0ec68a67e55c20ae3dde
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-04-13 16:53:20 +0200

    Cold breath VFX 1st iteration using player's velocity

---
+++ Snowy-Path/Assets/Scenes/Dev/NC/ColdBreath/BreathVelocity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+public class BreathVelocity : MonoBehaviour {
+
+    [SerializeField]
+    private VisualEffect m_coldBreath;
+    private ExposedProperty m_MyProperty;
+
+    [SerializeField]
+    private PlayerController m_playerController;
+
+
+    void Start() {
+        m_MyProperty = "PlayerVelocity"; // Assign A string
+    }
+
+    private void Update() {
+        m_coldBreath.SetVector3(m_MyProperty, (m_playerController.XZVelocity + m_playerController.ActualVelocity) * m_playerController.SpeedFactor);
+    }
+
+
+}
