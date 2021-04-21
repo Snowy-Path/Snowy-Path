b2d77d577f61a43d87ae7d7570428b0f392d5b92
Az <thomas.guyamier@gmail.com>
2021-04-21 15:47:10 +0200

    Added source change for light, and also spawn light for campfire

--- Snowy-Path/Assets/Scripts/Save/PlayerCampfireSave.cs
+++ Snowy-Path/Assets/Scripts/Save/PlayerCampfireSave.cs
@@ -37,6 +37,10 @@ public class PlayerCampfireSave : MonoBehaviour, ISaveable {
                         // If found the player transform is set as the last campfire respawn point
                         if (respawnTransform.gameObject.CompareTag("PlayerRespawnPoint"))
                         {
+                            if (item.lightForThisCampfire != null)
+                            {
+                                LightTransition.LightTransitionTo(item.lightForThisCampfire);
+                            }
                             CharacterController charController = GetComponent<CharacterController>();
                             PlayerController playerController = GetComponent<PlayerController>();
                             charController.enabled = false;
