fe30ae87f4c5f28dcf1d0a39da8ee3aa339ea5fd
Jer D <dambraij@gmail.com>
2021-04-29 12:54:19 +0200

    Make SHOTGUUUUN

--- Snowy-Path/Assets/Scripts/Tools/Handgun/Gun.cs
+++ Snowy-Path/Assets/Scripts/Tools/Handgun/Gun.cs
@@ -15,27 +15,30 @@ public class Gun : MonoBehaviour, IHandTool {
     public int ammunitionInInventory = 0;
     public int ammunitionInventoryLimit = 5;
 
-
     [Header("Gun")]
     public int ammoLoaded = 0;
     public int ammoLoadedLimit = 5;
     public float reloadingTime = 4;
 
+    public int projectilePerShot = 5;
     public int range = 1000;
+    public float spread = 1;
     public int damage = 0;
     public float fireRate = 0;
-    private bool shootReady = true;
 
 
     [Header("Settings")]
     public LayerMask ignoredLayers;
+    public GameObject impactPrefab;
     private Camera fpsCamera;
-
 
     [Header("AI")]
     // AI script for the hearing sense
     public HearingSenseEmitter emitter;
     public UnityEvent onEquip;
+
+    private bool shootReady = true;
+
 
     private void Awake() {
         fpsCamera = GetComponentInParent<Camera>();
@@ -60,23 +63,39 @@ public class Gun : MonoBehaviour, IHandTool {
     /// </summary>
     private void Shoot() {
         RaycastHit hit;
-        //If the ray hit something
-        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range, ignoredLayers)) {
-            //Set the corresponding endline point
-            if (hit.transform.CompareTag("Ennemy")) {
-                IEnnemyController ennemyController = hit.transform.GetComponent<IEnnemyController>();
-                if (ennemyController != null) {
-                    ennemyController.Hit(EToolType.Pistol, damage);
+
+        for (int i = 0; i < projectilePerShot; i++) {
+
+            float x = Random.Range(-spread, spread);
+            float y = Random.Range(-spread, spread);
+            float z = Random.Range(-spread, spread);
+            Vector3 randDirection = new Vector3(x, y, z);
+
+            //If the ray hit something
+            if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward + randDirection, out hit, range, ignoredLayers)) {
+
+                if (impactPrefab) {
+                    GameObject impactGo = Instantiate(impactPrefab, hit.point, Quaternion.Euler(hit.normal));
+                    Destroy(impactGo, 1f);
+                }
+
+                //Set the corresponding endline point
+                if (hit.transform.CompareTag("Ennemy")) {
+                    IEnnemyController ennemyController = hit.transform.GetComponent<IEnnemyController>();
+                    if (ennemyController != null) {
+                        ennemyController.Hit(EToolType.Pistol, damage);
+                    }
                 }
             }
         }
+
+        ammoLoaded--;
         handAnimator.SetTrigger("Shoot");
         onShoot.Invoke();
-        ammoLoaded--;
-        StartCoroutine(fireRateCoroutine());
+        StartCoroutine(FireRateCoroutine());
     }
 
-    IEnumerator fireRateCoroutine() {
+    IEnumerator FireRateCoroutine() {
         shootReady = false;
         yield return new WaitForSeconds(fireRate);
         shootReady = true;
