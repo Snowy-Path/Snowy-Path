c2898e1caf8a43c9949c275673413c66c03a7b0c
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-05-03 12:41:51 +0200

    The FireBowl now changes color when the puzzle is finished.

--- Snowy-Path/Assets/Scripts/Mechanics/BowlStelePuzzle.cs
+++ Snowy-Path/Assets/Scripts/Mechanics/BowlStelePuzzle.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,12 @@ public class BowlStelePuzzle : MonoBehaviour {
 
     public bool IsActive { private set; get; }
     private Transform m_child;
+    private ParticleSystem[] m_particleSystems;
 
     private void Start() {
-        m_child = transform.GetChild(0);
         IsActive = false;
+        m_child = transform.GetChild(0);
+        m_particleSystems = m_child.GetComponentsInChildren<ParticleSystem>(true);
     }
 
     public void SwitchActivation() {
@@ -19,5 +22,12 @@ public class BowlStelePuzzle : MonoBehaviour {
 
     internal void LightBowl() {
         m_child.gameObject.SetActive(true);
+    }
+
+    internal void ChangeParticleColor(Gradient color) {
+        foreach(var ps in m_particleSystems) {
+            var col = ps.colorOverLifetime;
+            col.color = color;
+        }
     }
 }
