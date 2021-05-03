07a882477c335a59d7e06468dc6f6fec93849b9b
Jer D <dambraij@gmail.com>
2021-05-03 15:48:27 +0200

    Add jump sounds & Start breath audio

---
+++ Snowy-Path/Assets/Scripts/Audio/ColdBreathAudio.cs
@@ -0,0 +1,24 @@
+﻿using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColdBreathAudio : MonoBehaviour
+{
+    [EventRef] [SerializeField] string lightBreath;
+    [EventRef] [SerializeField] string mediumBreath;
+    [EventRef] [SerializeField] string heavyBreath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
